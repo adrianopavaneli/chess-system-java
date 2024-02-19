c6575a2a1b6685ef31937213277abcddee8f57c2
Adriano Pavaneli <adriano.pavaneli.dev@gmail.com>
2024-02-18 21:11:10 -0300

    tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
+import xadrez.JogoXadrez;
+
 public class Program {
     public static void main(String[] args) {
-
+        JogoXadrez jogoXadrez = new JogoXadrez();
+        UI.imprimeTabuleiro(jogoXadrez.getPecas());
     }
 
 
