9db7567a03fb0b1039236e6e5b738b0a7c609d16
Adriano Pavaneli <adriano.pavaneli.dev@gmail.com>
2024-02-20 20:11:58 -0300

    movimentos rainha e bispo

--- src/xadrez/pecas/Bispo.java
+++ src/xadrez/pecas/Bispo.java
@@ -1,5 +1,6 @@
 package xadrez.pecas;
 
+import jogoTabuleiro.Posicao;
 import jogoTabuleiro.Tabuleiro;
 import xadrez.Cor;
 import xadrez.PecaXadrez;
@@ -16,7 +17,52 @@ public class Bispo extends PecaXadrez {
 
 
     public boolean[][] movimentosPossiveis() {
-        boolean[][] mat =  new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+        boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+        Posicao p = new Posicao(0, 0);
+
+        //noroeste
+
+        p.definirValor(posicao.getLinha() - 1, posicao.getColuna() - 1);
+        while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().haUmaPeca(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setLinha(p.getLinha() - 1);
+        }
+        if (getTabuleiro().posicaoExiste(p) && haPecaOponente(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        //nordeste
+
+        p.definirValor(posicao.getLinha() - 1, posicao.getColuna() + 1);
+        while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().haUmaPeca(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setLinha(p.getLinha() + 1);
+        }
+        if (getTabuleiro().posicaoExiste(p) && haPecaOponente(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        //sudoeste
+
+        p.definirValor(posicao.getLinha() + 1, posicao.getColuna() - 1);
+        while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().haUmaPeca(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setColuna(p.getColuna() + 1);
+        }
+        if (getTabuleiro().posicaoExiste(p) && haPecaOponente(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
+
+        //sudeste
+
+        p.definirValor(posicao.getLinha() + 1, posicao.getColuna() + 1);
+        while (getTabuleiro().posicaoExiste(p) && !getTabuleiro().haUmaPeca(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setColuna(p.getColuna() - 1);
+        }
+        if (getTabuleiro().posicaoExiste(p) && haPecaOponente(p)) {
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
         return mat;
     }
 }
--- src/xadrez/pecas/Peao.java
+++ src/xadrez/pecas/Peao.java
@@ -1,5 +1,6 @@
 package xadrez.pecas;
 
+import jogoTabuleiro.Posicao;
 import jogoTabuleiro.Tabuleiro;
 import xadrez.Cor;
 import xadrez.PecaXadrez;
@@ -15,6 +16,15 @@ public class Peao extends PecaXadrez {
     }
     public boolean[][] movimentosPossiveis() {
         boolean[][] mat =  new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+        Posicao p = new Posicao(0,0);
+
+        p.definirValor(posicao.getLinha() - 1, posicao.getColuna());
+            mat[p.getLinha()][p.getColuna()] = true;
+            p.setLinha(p.getLinha() -1);
+
+        if(getTabuleiro().posicaoExiste(p) && haPecaOponente(p)){
+            mat[p.getLinha()][p.getColuna()] = true;
+        }
         return mat;
     }
 }
